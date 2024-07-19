7180849ebf575f2fe6e4f53f5bbdbce0f1e0c273
Bruno Aybar <miguel_bruno@hotmail.com>
2024-07-19 00:06:22 -0500

    chore: initial commit

---
+++ site/build.gradle.kts
@@ -0,0 +1,43 @@
+import com.varabyte.kobweb.gradle.application.extensions.AppBlock.LegacyRouteRedirectStrategy
+import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kobweb.application)
+    alias(libs.plugins.kobwebx.markdown)
+}
+
+group = "web.android.dev.pe"
+version = "1.0-SNAPSHOT"
+
+kobweb {
+    app {
+        index {
+            description.set("Powered by Kobweb")
+        }
+
+        // Only legacy sites need this set. Sites built after 0.16.0 should default to DISALLOW.
+        // See https://github.com/varabyte/kobweb#legacy-routes for more information.
+        legacyRouteRedirectStrategy.set(LegacyRouteRedirectStrategy.DISALLOW)
+    }
+}
+
+kotlin {
+    configAsKobwebApplication("pe")
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+        }
+
+        jsMain.dependencies {
+            implementation(compose.html.core)
+            implementation(libs.kobweb.core)
+            implementation(libs.kobweb.silk)
+            implementation(libs.silk.icons.fa)
+            implementation(libs.kobwebx.markdown)
+            
+        }
+    }
+}
---
+++ site/src/jsMain/kotlin/web/android/dev/pe/AppEntry.kt
@@ -0,0 +1,20 @@
+package web.android.dev.pe
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.core.App
+import com.varabyte.kobweb.silk.SilkApp
+import com.varabyte.kobweb.silk.components.layout.Surface
+import com.varabyte.kobweb.silk.style.common.SmoothColorStyle
+import com.varabyte.kobweb.silk.style.toModifier
+import org.jetbrains.compose.web.css.*
+
+@App
+@Composable
+fun AppEntry(content: @Composable () -> Unit) {
+    SilkApp {
+        Surface(SmoothColorStyle.toModifier().minHeight(100.vh)) {
+            content()
+        }
+    }
+}
