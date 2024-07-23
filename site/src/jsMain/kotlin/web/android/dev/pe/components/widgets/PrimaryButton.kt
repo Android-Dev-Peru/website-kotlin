d3b6901e101527aa80bb71b74e838a0ed8ac1cad
Bruno Aybar <miguel_bruno@hotmail.com>
2024-07-23 02:17:24 -0500

    feat: add variants to primary button

--- site/src/jsMain/kotlin/web/android/dev/pe/components/widgets/PrimaryButton.kt
+++ site/src/jsMain/kotlin/web/android/dev/pe/components/widgets/PrimaryButton.kt
@@ -9,26 +9,28 @@ import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.navigation.Anchor
-import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.*
 import com.varabyte.kobweb.silk.style.selectors.hover
-import com.varabyte.kobweb.silk.style.toModifier
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.Text
 import web.android.dev.pe.Res
 
 @Composable
-fun PrimaryButton(text: String, href: String, modifier: Modifier = Modifier) {
-    Anchor(href = href, attrs = modifier.then(PrimaryButtonStyle.toModifier()).toAttrs()) {
+fun PrimaryButton(
+    text: String,
+    href: String,
+    variant: CssStyleVariant<PrimaryButtonKind> = FilledPrimaryButtonVariant,
+    modifier: Modifier = Modifier
+) {
+    Anchor(href = href, attrs = modifier.then(PrimaryButtonStyle.toModifier(variant)).toAttrs()) {
         Text(text)
     }
 }
 
-val PrimaryButtonStyle = CssStyle {
+sealed interface PrimaryButtonKind : ComponentKind
+val PrimaryButtonStyle = CssStyle<PrimaryButtonKind> {
     base {
         Modifier
-            .backgroundColor(Res.Theme.PRIMARY_BUTTON.color)
-            .color(Res.Theme.ON_PRIMARY_BUTTON.color)
-            .border { style(LineStyle.None) }
             .padding(20.px)
             .textAlign(TextAlign.Center)
             .textDecorationLine(TextDecorationLine.None)
@@ -42,5 +44,33 @@ val PrimaryButtonStyle = CssStyle {
         Modifier
             .backgroundColor(Res.Theme.BUTTON_HOVER.color)
             .color(Colors.Black)
+    }
+}
+
+val FilledPrimaryButtonVariant = PrimaryButtonStyle.addVariant {
+    base {
+        Modifier
+            .backgroundColor(Res.Theme.PRIMARY_BUTTON.color)
+            .color(Res.Theme.ON_PRIMARY_BUTTON.color)
+            .border { style(LineStyle.None) }
+    }
+    hover {
+        Modifier
+            .backgroundColor(Res.Theme.BUTTON_HOVER.color)
+            .color(Colors.Black)
+    }
+}
+
+val OutlinePrimaryButtonVariant = PrimaryButtonStyle.addVariant {
+    base {
+        Modifier
+            .border(2.px, LineStyle.Solid, Colors.Black)
+            .borderRadius(96.px)
+            .color(Colors.Black)
+    }
+    hover {
+        Modifier
+            .backgroundColor(Colors.Black)
+            .color(Colors.White)
     }
 }
