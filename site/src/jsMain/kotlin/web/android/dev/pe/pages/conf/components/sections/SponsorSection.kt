100cd006fb8221e7c9d21a62efe18e8f961e970a
Bruno Aybar <miguel_bruno@hotmail.com>
2024-08-07 13:40:45 -0500

    feat: make the landing sponsorship link redirect to the sponsorship page

--- site/src/jsMain/kotlin/web/android/dev/pe/pages/conf/components/sections/SponsorSection.kt
+++ site/src/jsMain/kotlin/web/android/dev/pe/pages/conf/components/sections/SponsorSection.kt
@@ -13,17 +13,23 @@ import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
 import strings.ResStrings
 import web.android.dev.pe.Res
+import web.android.dev.pe.components.utils.appendCurrentLanguage
 import web.android.dev.pe.components.widgets.OutlinePrimaryButtonVariant
 import web.android.dev.pe.components.widgets.PrimaryButton
 import web.android.dev.pe.pages.conf.components.layouts.ConferenceGridSection
 
 @Composable
-fun SponsorSection(sectionModifier: Modifier = Modifier, modifier: Modifier = Modifier) {
+fun SponsorSection(
+    ctaTitle: String,
+    ctaPath: String,
+    sectionModifier: Modifier = Modifier,
+    modifier: Modifier = Modifier,
+) {
     ConferenceGridSection(
         sectionModifier = sectionModifier,
         modifier = modifier,
         header = {
-            Details()
+            Details(ctaTitle, ctaPath)
         },
         content = {
             Sponsors()
@@ -33,12 +39,12 @@ fun SponsorSection(sectionModifier: Modifier = Modifier, modifier: Modifier = Mo
 
 
 @Composable
-private fun Details() {
+private fun Details(ctaTitle: String, ctaPath: String) {
     H2 { Text(ResStrings.conf_sponsor_title) }
     P { Text(ResStrings.conf_sponsor_caption) }
     PrimaryButton(
-        text = ResStrings.conf_sponsor_cta,
-        href = Res.Links.Conf.SponsorBrief,
+        text = ctaTitle,
+        href = ctaPath,
         modifier = Modifier.margin(topBottom = 16.px),
         variant = OutlinePrimaryButtonVariant,
     )
