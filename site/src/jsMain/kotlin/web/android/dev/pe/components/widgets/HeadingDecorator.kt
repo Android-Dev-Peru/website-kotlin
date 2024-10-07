b18b0abef670664fe238505b677d6b960cca6897
Bruno Aybar <miguel_bruno@hotmail.com>
2024-10-07 00:15:15 -0500

    WIP

--- site/src/jsMain/kotlin/web/android/dev/pe/components/widgets/HeadingDecorator.kt
+++ site/src/jsMain/kotlin/web/android/dev/pe/components/widgets/HeadingDecorator.kt
@@ -2,11 +2,12 @@ package web.android.dev.pe.components.widgets
 
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Color
 import com.varabyte.kobweb.compose.ui.modifiers.color
 import com.varabyte.kobweb.silk.components.text.SpanText
 import web.android.dev.pe.Theme
 
 @Composable
-fun HeadingDecorator() {
-    SpanText("▮ ", Modifier.color(Theme.LIGHT_GREEN))
+fun HeadingDecorator(color: Color.Rgb = Theme.LIGHT_GREEN) {
+    SpanText("▮ ", Modifier.color(color))
 }
